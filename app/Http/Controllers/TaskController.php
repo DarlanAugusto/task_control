b925dbd2419461691c565143a39e8645b98c5ada
Darlan <darlansilv4@gmail.com>
2023-07-24 21:58:04 -0300

    feat: Criando controller, model, e rotas padrões para as Tarefas

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return "Olá " . Auth::user()->name . ", chegamos até aqui!";
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Task $task)
+    {
+        //
+    }
+
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +22,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->resource('/task', TaskController::class);
