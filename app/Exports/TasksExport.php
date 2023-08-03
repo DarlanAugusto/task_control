b0c525b73f8561270179f289740df0799ed0b1b7
Darlan <darlansilv4@gmail.com>
2023-08-03 01:00:32 -0300

    feat: implementando interface WithHeadings

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TasksExport implements FromCollection
+class TasksExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,17 @@ class TasksExport implements FromCollection
     public function collection()
     {
         return Auth::user()->tasks()->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Usuário',
+            'Tarefa',
+            'Data limite',
+            'Data criação',
+            'Data Conclusão'
+        ];
     }
 }
